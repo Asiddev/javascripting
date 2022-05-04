ef14c78878aeb705b5fb76368d0dc82faf059b8e
asiddev <AsidDev@protonmail.com>
2022-05-04 02:51:21 +0000

    Initial commit with all my work so far

---
+++ number-to-string.js
@@ -0,0 +1,9 @@
+var n = 128;
+n = n.toString();
+console.log(n);
+
+
+
+
+
+
---
+++ tinkering.js
@@ -0,0 +1,8 @@
+var trash = "";
+var bins = {
+  waste: 4,
+  recycling: 3,
+  compost: 5,
+};
+
+smartGarbage = function (trash, bins) {};
